ee57a37bba8c951a58bf2357f6b6257872c33ea2
fbedir <ferhat.bedir@emlakjet.com>
2023-11-20 15:43:45 +0300

    created enty and transfer dao layer and unit tests

---
+++ db/sqlc/entries_test.go
@@ -0,0 +1,90 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func createRandomEntry(t *testing.T) Entry {
+	account := createRandomAccount(t)
+	arg := CreateEntriesParams{
+		AccountID: account.ID,
+		Amount:    account.Balance / 2,
+	}
+
+	entry, err := testQueries.CreateEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntries(t *testing.T) {
+	entry := createRandomEntry(t)
+	foundEntry, err := testQueries.GetEntries(context.Background(), entry.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, foundEntry)
+
+	require.Equal(t, entry.ID, foundEntry.ID)
+	require.Equal(t, entry.AccountID, foundEntry.AccountID)
+	require.Equal(t, entry.Amount, foundEntry.Amount)
+	require.WithinDuration(t, entry.CreatedAt, foundEntry.CreatedAt, time.Second)
+}
+
+func TestUpdateEntry(t *testing.T) {
+	entry := createRandomEntry(t)
+	arg := UpdateEntriesParams{
+		ID:     entry.ID,
+		Amount: entry.Amount / 2,
+	}
+	updatedEntry, err := testQueries.UpdateEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedEntry)
+
+	require.Equal(t, entry.ID, updatedEntry.ID)
+	require.Equal(t, entry.AccountID, updatedEntry.AccountID)
+	require.Equal(t, arg.Amount, updatedEntry.Amount)
+	require.WithinDuration(t, entry.CreatedAt, updatedEntry.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	entry := createRandomEntry(t)
+	err := testQueries.DeleteEntries(context.Background(), entry.ID)
+	require.NoError(t, err)
+
+	notFoundEntry, err := testQueries.GetEntries(context.Background(), entry.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, notFoundEntry)
+}
+
+func TestListsEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomEntry(t)
+	}
+
+	arg := ListEntriesParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
---
+++ db/sqlc/transfers_test.go
@@ -0,0 +1,95 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func createRandomTransfer(t *testing.T) Transfer {
+	fromAccount := createRandomAccount(t)
+	toAccount := createRandomAccount(t)
+	arg := CreateTransfersParams{
+		FromAccountID: fromAccount.ID,
+		ToAccountID:   toAccount.ID,
+		Amount:        fromAccount.Balance / 2,
+	}
+
+	transfer, err := testQueries.CreateTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	createRandomTransfer(t)
+}
+
+func TestGetTransfer(t *testing.T) {
+	transfer := createRandomTransfer(t)
+	foundTransfer, err := testQueries.GetTransfer(context.Background(), transfer.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, foundTransfer)
+
+	require.Equal(t, transfer.ID, foundTransfer.ID)
+	require.Equal(t, transfer.FromAccountID, foundTransfer.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, foundTransfer.ToAccountID)
+	require.Equal(t, transfer.Amount, foundTransfer.Amount)
+	require.WithinDuration(t, transfer.CreatedAt, foundTransfer.CreatedAt, time.Second)
+}
+
+func TestUpdateTransfer(t *testing.T) {
+	transfer := createRandomTransfer(t)
+	arg := UpdateTransferParams{
+		ID:     transfer.ID,
+		Amount: transfer.Amount / 2,
+	}
+	updatedTransfer, err := testQueries.UpdateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedTransfer)
+
+	require.Equal(t, transfer.ID, updatedTransfer.ID)
+	require.Equal(t, transfer.FromAccountID, updatedTransfer.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, updatedTransfer.ToAccountID)
+	require.Equal(t, arg.Amount, updatedTransfer.Amount)
+	require.WithinDuration(t, transfer.CreatedAt, updatedTransfer.CreatedAt, time.Second)
+}
+
+func TestDeleteTransfer(t *testing.T) {
+	transfer := createRandomTransfer(t)
+	err := testQueries.DeleteTransfer(context.Background(), transfer.ID)
+	require.NoError(t, err)
+
+	notFoundTransfer, err := testQueries.GetTransfer(context.Background(), transfer.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, notFoundTransfer)
+}
+
+func TestListsTransfers(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t)
+	}
+
+	arg := ListTransfersParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	transfers, err := testQueries.ListTransfers(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+	}
+}
